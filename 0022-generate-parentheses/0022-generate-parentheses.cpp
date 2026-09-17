ed00a8e4648823357ddcf423d38ffcad86d4f31f
omkar7goat <150643814+omkar7goat@users.noreply.github.com>
2026-09-18 01:06:18 +0530

    Time: 3 ms (67.58%), Space: 15.9 MB (28.73%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-   vector<string>v;
-    void helper(int n,int a,int b,string s){
-    if(s.size()==2*n){
-        v.push_back(s);return;
+    vector<string>v;
+    void f(string s , int o , int c ,int n){
+        if((o+c==2*n)&&(o==c)){
+            v.push_back(s);return;
+        } 
+        if(o<n)f(s+'(',o+1,c,n);      
+        if(c<o) f(s+')',o,c+1,n);
+       
     }
-    if(a<n){
-        helper(n,a+1,b,s+'(');
-    }
-    if(b<a){
-        helper(n,a,b+1,s+')');
-    }
- }
     vector<string> generateParenthesis(int n) {
-        helper(n,0,0,"");
+        f("",0,0,n);
         return v;
     }
 };
